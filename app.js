faa2d15b238d743924f89319d3548242e761c91e
Melissa <melissakheying@gmail.com>
2020-10-12 08:39:51 -0500

    use express set engine to view HTML for home-guest file

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const express = require('express')
 const app = express()
 
+// first 'views' is strictly an express option, second 'views' is the name of our folder
+app.set('views', 'views')
+// telling express to use the ejs engine (npm i ejs)
+app.set('view engine', 'ejs')
+
 //express is doing a get req to the base URL
 app.get('/', (req, res) => {
-  res.send('Welcome to our new App')
+  res.render('home-guest')
 })
 
 app.listen(3000)
