b77660ed2013375cff89461bf035fcfe3ab74a7c
Melissa <melissakheying@gmail.com>
2020-10-12 09:38:29 -0500

    :construction: create controllers to better organize the use of router

--- router.js
+++ router.js
@@ -1,10 +1,8 @@
 const express = require('express')
-
 const router = express.Router()
+const userController = require('./controllers/userController')
 
 // creating a route to display home-guest HTML file when the user goes to the base URL http://locoalhost:3000/
-router.get('/', (req, res) => {
-  res.render('home-guest')
-})
+router.get('/', userController.home)
 
 module.exports = router
