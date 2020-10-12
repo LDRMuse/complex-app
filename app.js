d2a0f27f60c274f11d147b10dfc075c82322f909
Melissa <melissakheying@gmail.com>
2020-10-12 08:43:38 -0500

    use express to make public folder accessible

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const app = express()
 
+// express is making public folder accessible
+app.use(express.static('public'))
+
 // first 'views' is strictly an express option, second 'views' is the name of our folder
-app.set('views', 'views')
 // telling express to use the ejs engine (npm i ejs)
+app.set('views', 'views')
 app.set('view engine', 'ejs')
 
 //express is doing a get req to the base URL
