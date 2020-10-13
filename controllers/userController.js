a7ea8b49f1c1b90fcaa8bce5278e5db3beaf4f79
Melissa <melissakheying@gmail.com>
2020-10-13 07:54:51 -0500

    :sparkles: setup validation for registering (cannot use arrow functions)
    arrow functions do not have the prototypes

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,19 +1,23 @@
 const User = require('../models/User')
 
-exports.login = () => {
+exports.login = function() {
 
 }
 
-exports.logout = () => {
+exports.logout = function() {
 
 }
 
-exports.register = (req, res) => {
+exports.register = function(req, res) {
   let user = new User(req.body)
   user.register()
-  res.send('THanks for registers')
+  if (user.errors.length) {
+    res.send(user.errors)
+  } else {
+      res.send('Congrats, no errors')
+    }
 }
 
-exports.home = (req, res) => {
+exports.home = function(req, res) {
   res.render('home-guest')
 }
--- models/User.js
+++ models/User.js
@@ -1,13 +1,24 @@
+//This file as (User) gets called by the userController
+
 
 // storing user input
-let User = (data) => {
+let User = function(data) {
 this.data = data
-
+this.errors = []
 }
 
-User.prototype.register = () => {
 
+User.prototype.validate = function() {
+if (this.data.username = '') {this.errors.push('You must provide a username')}
+if (this.data.email = '') {this.errors.push('You must provide a valid email address')}
+if (this.data.password = '') {this.errors.push('You must provide a password')}
+}
 
+User.prototype.register = function() {
+// step 1. Validate user data
+this.validate()
+// step 2. Only if there are no errors,
+// then save user data into database
 }
 
 
