b6a81acc70c38f8d596a5d89f5568fb97af2e949
Melissa <melissakheying@gmail.com>
2020-10-13 08:03:05 -0500

    :bug: replace assignment = with the equality operator ===

--- models/User.js
+++ models/User.js
@@ -9,9 +9,9 @@ this.errors = []
 
 
 User.prototype.validate = function() {
-if (this.data.username = '') {this.errors.push('You must provide a username')}
-if (this.data.email = '') {this.errors.push('You must provide a valid email address')}
-if (this.data.password = '') {this.errors.push('You must provide a password')}
+if (this.data.username === "") {this.errors.push('You must provide a username')}
+if (this.data.email === "") {this.errors.push('You must provide a valid email address')}
+if (this.data.password === "") {this.errors.push('You must provide a password')}
 }
 
 User.prototype.register = function() {
