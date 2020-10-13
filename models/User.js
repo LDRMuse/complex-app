c002f08bf2c76d7233a941ab64882b342ce0fe9b
Melissa <melissakheying@gmail.com>
2020-10-13 09:48:59 -0500

    :bug: place ( ) in right area

--- models/User.js
+++ models/User.js
@@ -1,21 +1,17 @@
-//This file as (User) gets called by the userController
+const usersCollection = require('../db').collection("users")
+const validator = require("validator")
 
-const usersCollection = require('../db').collection('users')
-const validator = require('validator')
-
-
-// storing user input
-let User = function (data) {
+let User = function(data) {
   this.data = data
   this.errors = []
 }
 
-User.prototype.cleanUp = function () {
-  if (typeof(this.data.username != "string")) {this.data.username = ""}
-  if (typeof(this.data.email != "string")) {this.data.email = ""}
-  if (typeof(this.data.password != "string")) {this.data.password = ""}
+User.prototype.cleanUp = function() {
+  if (typeof(this.data.username) != "string") {this.data.username = ""}
+  if (typeof(this.data.email) != "string") {this.data.email = ""}
+  if (typeof(this.data.password) != "string") {this.data.password = ""}
 
-  // only accept these properties
+  // get rid of any bogus properties
   this.data = {
     username: this.data.username.trim().toLowerCase(),
     email: this.data.email.trim().toLowerCase(),
@@ -24,27 +20,26 @@ User.prototype.cleanUp = function () {
 }
 
 User.prototype.validate = function() {
-  if (this.data.username === "") { this.errors.push('You must provide a username') }
-  if (this.data.username != "" && !validator.isAlphanumeric(this.data.username)) { this.errors.push('Username can only contain letters and numbers.')}
-  if (this.data.username.length > 0 && this.data.username.length < 3) { this.errors.push('Username must be at least 3 characters') }
-  if (this.data.username.length > 30) { this.errors.push('Username cannot exceed 30 characters.') }
-  if (!validator.isEmail(this.data.email)) { this.errors.push('You must provide a valid email address') }
-  if (this.data.password === "") { this.errors.push('You must provide a password') }
-  if (this.data.password.length > 0 && this.data.password.length < 12) { this.errors.push('Password must be at least 12 characters') }
-  if (this.data.password.length > 100) { this.errors.push('Password cannot exceed 100 characters.') }
-
+  if (this.data.username == "") {this.errors.push("You must provide a username.")}
+  if (this.data.username != "" && !validator.isAlphanumeric(this.data.username)) {this.errors.push("Username can only contain letters and numbers.")}
+  if (!validator.isEmail(this.data.email)) {this.errors.push("You must provide a valid email address.")}
+  if (this.data.password == "") {this.errors.push("You must provide a password.")}
+  if (this.data.password.length > 0 && this.data.password.length < 12) {this.errors.push("Password must be at least 12 characters.")}
+  if (this.data.password.length > 100) {this.errors.push("Password cannot exceed 100 characters.")}
+  if (this.data.username.length > 0 && this.data.username.length < 3) {this.errors.push("Username must be at least 3 characters.")}
+  if (this.data.username.length > 30) {this.errors.push("Username cannot exceed 30 characters.")}
 }
 
-User.prototype.register = function () {
-  // step 1. Validate user data
+User.prototype.register = function() {
+  // Step #1: Validate user data
   this.cleanUp()
   this.validate()
-  // step 2. Only if there are no errors,
-  // then save user data into database
-if (!this.errors.length) {
-  usersCollection.insertOne(this.data)
-}
-}
 
+  // Step #2: Only if there are no validation errors
+  // then save the user data into a database
+  if (!this.errors.length) {
+    usersCollection.insertOne(this.data)
+  }
+}
 
 module.exports = User
