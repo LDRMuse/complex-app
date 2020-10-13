df1d814268e4efe28a93e345bb3a62ec3d5909db
Melissa <melissakheying@gmail.com>
2020-10-13 09:35:38 -0500

    set up db connection for users collection

---
+++ db.js
@@ -0,0 +1,12 @@
+const mongodb = require('mongodb')
+const dotenv = require('dotenv')
+
+dotenv.config();
+
+const connectionString = process.env.MONGO_USERS_URI
+
+mongodb.connect(connectionString, { useNewUrlParser: true, useUnifiedTopology: true }, function (err, client) {
+  module.exports = client.db()
+  const app = require('./app')
+  app.listen(process.env.PORT)
+})
