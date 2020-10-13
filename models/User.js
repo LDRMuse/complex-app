cd9180dc9c1d24a20786b7aac38010cb98ef1a86
Melissa <melissakheying@gmail.com>
2020-10-13 08:35:44 -0500

    :sparkles: create a cleanUp function to prevent bogus properties

--- models/User.js
+++ models/User.js
@@ -9,8 +9,20 @@ let User = function (data) {
   this.errors = []
 }
 
+User.prototype.cleanUp = function () {
+  if (typeof(this.data.username != "string")) {this.data.username = ""}
+  if (typeof(this.data.email != "string")) {this.data.email = ""}
+  if (typeof(this.data.password != "string")) {this.data.password = ""}
 
-User.prototype.validate = function () {
+  // only accept these properties
+  this.data = {
+    username: this.data.username.trim().toLowerCase(),
+    email: this.data.email.trim().toLowerCase(),
+    password: this.data.password
+  }
+}
+
+User.prototype.validate = function() {
   if (this.data.username === "") { this.errors.push('You must provide a username') }
   if (this.data.username != "" && !validator.isAlphanumeric(this.data.username)) { this.errors.push('Username can only contain letters and numbers.')}
   if (this.data.username.length > 0 && this.data.username.length < 3) { this.errors.push('Username must be at least 3 characters') }
@@ -24,6 +36,7 @@ User.prototype.validate = function () {
 
 User.prototype.register = function () {
   // step 1. Validate user data
+  this.cleanUp()
   this.validate()
   // step 2. Only if there are no errors,
   // then save user data into database
