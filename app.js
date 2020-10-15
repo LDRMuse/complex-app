c3c41eff41c7dd54032485709e64a3b7c395fdb2
Melissa <melissakheying@gmail.com>
2020-10-15 14:11:16 -0500

    reference express session package

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const session = require('express-session')
-const MongoStore = require('connect-mongo')
+const MongoStore = require('connect-mongo')(session)
 const app = express()
 
 let sessionOptions = session({
