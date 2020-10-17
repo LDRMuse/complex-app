a05494855b223341862068885545c0b82d0ba3db
Melissa <melissakheying@gmail.com>
2020-10-17 11:17:41 -0500

    create error message if username already exists
    first create if statement to check if username is actually valid. Once username proves valid, create a variable called usernameExists. usernameExists is assigned by taking the usersCollection from mongodb and finding  the first document where the username matches what the user tried to register with (this.data.username). If mongodb does find a matching document, then we will want to display the error message "That username is already taken". If username does not exist, the object will evaluate to null and allow user to sign up. *first make the entire function a promise (async/await) to ensure the correct timing of the if statements for mongodb.

--- models/User.js
+++ models/User.js
@@ -45,7 +45,7 @@ User.prototype.login = function () {
 }
 
 
-User.prototype.validate = function () {
+User.prototype.validate = async function () {
   if (this.data.username == "") { this.errors.push("You must provide a username.") }
   if (this.data.username != "" && !validator.isAlphanumeric(this.data.username)) { this.errors.push("Username can only contain letters and numbers.") }
   if (!validator.isEmail(this.data.email)) { this.errors.push("You must provide a valid email address.") }
@@ -54,6 +54,12 @@ User.prototype.validate = function () {
   if (this.data.password.length > 50) { this.errors.push("Password cannot exceed 50 characters.") }
   if (this.data.username.length > 0 && this.data.username.length < 3) { this.errors.push("Username must be at least 3 characters.") }
   if (this.data.username.length > 30) { this.errors.push("Username cannot exceed 30 characters.") }
+
+  // only if username is valid, then check to see if it's already taken
+  if (this.data.username.length > 2 && this.data.username.length < 31 && validator.isAlphanumeric(this.data.username)) {
+    let usernameExists = await usersCollection.findOne({ username: this.data.username })
+    if (usernameExists) { this.errors.push("That username is already taken") }
+  }
 }
 
 User.prototype.register = function () {
