bf3c65a2644043769aaa2f275ce664c06701b8dd
Melissa <melissakheying@gmail.com>
2020-10-19 17:37:27 -0500

    store values in database from the form fields by the user
    start with creating a function in the postController called create. The actual function logic needs to be held in the model. So we created a model called Post.js

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,3 +1,8 @@
 exports.viewCreateScreen = function (req, res) {
   res.render('create-post')
 }
+
+exports.create = function (req, res) {
+  // store values in database that were typed into the form fields by the user
+
+}
