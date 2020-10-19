5069b99da7c722f86d0668a071a8b3e123fdfb89
Melissa <melissakheying@gmail.com>
2020-10-19 12:02:43 -0500

    :sparkles: create function & use md5 to hash users avatar (GRAVATAR URL)

--- models/User.js
+++ models/User.js
@@ -95,7 +95,7 @@ User.prototype.register = function () {
 }
 
 User.prototype.getAvatar = function () {
-  this.avatar = `https://gravatar.com/avatar/${email}?s=128`
+  this.avatar = `https://gravatar.com/avatar/${md5(this.data.email)}?s=128`
 }
 
 
