b535b854c05d4208f013be288eefb09c5fc96fe7
Melissa <melissakheying@gmail.com>
2020-10-19 15:02:03 -0500

    :sparkles: create a route when user clicks on 'Create Post' Button
    using a GET request to display the create-post HTML by rendering the ejs

--- router.js
+++ router.js
@@ -1,13 +1,15 @@
 const express = require('express')
 const router = express.Router()
 const userController = require('./controllers/userController')
+const postController = require('./controllers/postController')
 
-// creating routes set up in home-guest HTML file
-//to display content when the user goes to certain URLs
-// userController holds a collection of factory functions
+// user related routes
 router.get('/', userController.home)
 router.post('/register', userController.register)
 router.post('/login', userController.login)
 router.post('/logout', userController.logout)
+
+// post related routes
+router.get('/create-post', postController.viewCreateScreen)
 
 module.exports = router
