f55b451bc16d40bca6766c9e0f97f9081f969a8d
Melissa <melissakheying@gmail.com>
2020-10-19 18:00:34 -0500

    create Post functions validate & cleanUp to use in the create function
    the validate function only allows strings. Any errors gets push into the errors array. The cleanUp function only allows the specific object data and ignores any bogus entries.

--- models/Post.js
+++ models/Post.js
@@ -1,7 +1,27 @@
 let Post = function (data) {
-this.data = data
+  this.data = data
+  this.errors = []
 }
 
+Post.prototype.cleanUp = function () {
+  if (typeof (this.data.title) != 'string') { this.data.title = '' }
+  if (typeof (this.data.body) != 'string') { this.data.body = '' }
 
+  // only allow the specific properties in the object
+  this.data = {
+    title: this.data.title.trim(),
+    body: this.data.body.trim(),
+    createdDate: new Date()
+  }
+}
+
+Post.prototype.validate = function () {
+  if (this.data.title === '') { this.errors.push('You must provide a title.') }
+  if (this.data.body === '') { this.errors.push('You must provide post content.') }
+}
+
+Post.prototype.create = function () {
+
+}
 
 module.exports = Post
