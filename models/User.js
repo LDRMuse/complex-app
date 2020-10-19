478529e56f7cd22ec808944649500ce8b3f98b70
Melissa <melissakheying@gmail.com>
2020-10-19 11:59:38 -0500

    :building_construction: npm install md5 and bring it in

--- models/User.js
+++ models/User.js
@@ -1,6 +1,7 @@
 const bcrypt = require('bcryptjs')
 const usersCollection = require('../db').db().collection("users")
 const validator = require("validator")
+const md5 = require('md5')
 
 let User = function (data) {
   this.data = data
