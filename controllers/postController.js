02423724956395fe1c8bd60a35c2a19a16e478b3
Melissa <melissakheying@gmail.com>
2020-10-19 17:42:43 -0500

    require in the Post.js Model (blueprint for creating post objects)
    leverage the blueprint or model of what the post should be. Pass blueprint (req.body) which contains the form data that the user just submitted.

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,8 +1,10 @@
+const Post = require('../models/Post')
+
+
 exports.viewCreateScreen = function (req, res) {
   res.render('create-post')
 }
 
 exports.create = function (req, res) {
-  // store values in database that were typed into the form fields by the user
-
+  let post = new Post(req.body)
 }
