b8334d0ba185e96ae3370771e4bf5cf24551dff9
Melissa <melissakheying@gmail.com>
2020-10-23 12:18:35 -0500

    :pencil: adjust note to make more sense

--- frontend-js/modules/search.js
+++ frontend-js/modules/search.js
@@ -1,5 +1,18 @@
 export default class Search {
   constructor() {
-    alery('search js is successfully being exicuted')
+
   }
 }
+
+// class is syntactic sugar for prototypes
+// Below is the same thing as above but the above only works in the front end
+
+// function Search() {
+//   this.exampleProperty = x
+// }
+
+// Search.prototype.myCoolMethod = function() {
+
+// }
+
+// new Search()
