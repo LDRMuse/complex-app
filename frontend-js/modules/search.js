708eb45db23bdd60f90bf66719db4505cc9c9770
Melissa <melissakheying@gmail.com>
2020-10-24 09:11:48 -0500

    :sparkles: add an alert message when search bar icon is clicked
    set up 4 steps. 1. is to select DOM elements within the constructor In this case we are selecting the header-search-icon class. 2. set up the event for this element. we add an event listener, click and prevent default. then we call the actual method 3. openOverlay is the actual method that has the alert. in order for the final result of the alert popping up, we need to do step 4. call the events function.

--- frontend-js/modules/search.js
+++ frontend-js/modules/search.js
@@ -1,18 +1,19 @@
 export default class Search {
+  // 1. Select DOM elements and keep track of any useful data
   constructor() {
-
+    this.headerSearchIcon = document.querySelector(".header-search-icon")
+    // 4. have the constructor call the events
+    this.events()
+  }
+  // 2. Events such as clicking, scrolling, user hitting keys on the keyboard
+  events() {
+    this.headerSearchIcon.addEventListener("click", (e) => {
+      e.preventDefault()
+      this.openOverlay()
+    })
+  }
+  //3. Methods
+  openOverlay() {
+    alert('openOverlay method just ran!!!!')
   }
 }
-
-// class is syntactic sugar for prototypes
-// Below is the same thing as above but the above only works in the front end
-
-// function Search() {
-//   this.exampleProperty = x
-// }
-
-// Search.prototype.myCoolMethod = function() {
-
-// }
-
-// new Search()
